3b921565e706b4035f0a60f34465f017d1982c49
Robbie Bardijn <robbie.bardijn@gmail.com>
2019-02-03 23:58:08 +0100

    master: colors

--- src/components/squad/Member.js
+++ src/components/squad/Member.js
@@ -2,31 +2,26 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Headings } from '../../elements';
 
-
 export class Member extends Component {
   render() {
     const { member } = this.props;
-    if(member == null) return <div>empty</div>;
+    if (member == null) return <div>empty</div>;
     return (
-      <StyledMember>
-        <img
-          src={member.image}
-          alt={member.name}
-          className="image"
-        />
+      <StyledMember member={member}>
+        <img src={member.image} alt={member.name} className="image" />
         <Headings.h2 className="name">{member.name}</Headings.h2>
       </StyledMember>
     );
   }
 }
 const StyledMember = styled.div`
+  .image {
+    width: 100%;
+  }
   .name {
-    color: #FFF;
+    color: #fff;
     font-size: 1.25rem;
   }
 `;
-
-
-
 
 export default Member;
--- src/components/squad/Squad.js
+++ src/components/squad/Squad.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Headings } from '../../elements';
-import { Colors } from '../../utilities';
+import { Colors, TypeColors } from '../../utilities';
 import Member from './Member';
 
 export class Squad extends Component {
@@ -12,8 +12,8 @@ export class Squad extends Component {
       <div>
         <Headings.h2>Selected Squad</Headings.h2>
         <SquadList>
-          {members.map((member,i) => (
-            <SquadListItem key={i}>
+          {members.map((member, i) => (
+            <SquadListItem key={i} member={member}>
               <Member member={member} />
             </SquadListItem>
           ))}
@@ -37,6 +37,14 @@ const SquadListItem = styled.li`
   align-items: center;
   background: ${Colors.grey};
   color: ${Colors.blue};
+
+  ${({ member }) => {
+    if(member) {
+      return `
+        background: ${TypeColors[member.types[0].name]}
+      `;
+    }
+  }}
 `;
 
 export default Squad;
